0f2cf53af914b447cebd08c5666066c31e72c308
Pari <pari.thanponsri@gmail.com>
2025-11-23 17:56:21 +0700

    Create lab1.2.java

---
+++ lab/lab1/lab1.2.java
@@ -0,0 +1,14 @@
+package lab1_2;
+import java.util.Scanner;
+public class lab1_2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		double Width = input.nextDouble();
+		double Height = input.nextDouble();
+		double Area;
+		Area = Width * Height;
+		System.out.println(Area);
+	}
+	
+}
