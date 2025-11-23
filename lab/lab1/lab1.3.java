063c0dd5fce71873a5a47a7838523a4ab744df31
Pari <pari.thanponsri@gmail.com>
2025-11-23 18:01:46 +0700

    Rename class and package for lab1.3

--- lab/lab1/lab1.3.java
+++ lab/lab1/lab1.3.java
@@ -1,15 +1,16 @@
-package lab1_3;
+package lab1_14;
 import java.util.Scanner;
-public class lab1_3 {
+
+public class lab1_14 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		int number = input.nextInt();
 		if (number % 2 == 0) {
-			System.out.println("This number is Even");
+			System.out.println("Even");
 		} else {
-			System.out.println("This number is Odd");
+			System.out.println("Odd");
 		}
 	}
 
