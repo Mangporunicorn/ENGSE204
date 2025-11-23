81aba7dadc22fc5499f19771731940616bf448df
Pari <pari.thanponsri@gmail.com>
2025-11-23 17:25:35 +0700

    Create lab1.1.java

---
+++ lab/lab1/lab1.1.java
@@ -0,0 +1,16 @@
+Lab1.1
+
+package lab1_1;
+import java.util.Scanner;
+public class lab1_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int number1 = input.nextInt();
+		int number2 = input.nextInt();
+		int sum;
+		sum = number1 + number2;
+		System.out.println(sum);
+	}	
+}
