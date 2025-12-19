07466ec1bca484d621491ac217d644701199169c
Pari <pari.thanponsri@gmail.com>
2025-12-19 14:10:18 +0700

    Create lab4.12.java

---
+++ lab/lab4/lab4.12.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+class Schedule {
+    private int hour;
+
+    public Schedule(int hour) {
+        this.hour = hour;
+    }
+
+    public Schedule(Schedule other) {
+        this.hour = other.hour;
+    }
+
+    public void setHour(int newHour) {
+        this.hour = newHour;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+}
+
+class EmployeeSchedule {
+    private String name;
+    private Schedule schedule;
+
+    public EmployeeSchedule(String name, Schedule schedule) {
+        this.name = name;
+        this.schedule = schedule;
+    }
+
+    public EmployeeSchedule(EmployeeSchedule other) {
+        this.name = other.name;
+        this.schedule = new Schedule(other.schedule);
+    }
+
+    public void displaySchedule() {
+        System.out.println(name + ": " + schedule.getHour() + ":00");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        int startHour = sc.nextInt();
+        int newHour = sc.nextInt();
+
+        Schedule sch1 = new Schedule(startHour);
+
+        EmployeeSchedule emp1 = new EmployeeSchedule(name, sch1);
+
+        EmployeeSchedule emp2 = new EmployeeSchedule(emp1);
+
+        sch1.setHour(newHour);
+
+        emp1.displaySchedule();
+        emp2.displaySchedule();
+    }
+}
