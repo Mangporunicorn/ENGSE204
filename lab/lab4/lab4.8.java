7bd422ab871910109c860b775b02aae7cfb300a0
Pari <pari.thanponsri@gmail.com>
2025-12-19 13:45:25 +0700

    Create lab4.8.java

---
+++ lab/lab4/lab4.8.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+class Movie {
+
+    private String title;
+    private String director;
+    private double rating;
+
+    public Movie(String title) {
+        this(title, "Unknown");   // เรียก constructor ตัวที่ 2
+    }
+
+    public Movie(String title, String director) {
+        this(title, director, 0.0);   // เรียก constructor ตัวที่ 3
+    }
+
+    public Movie(String title, String director, double rating) {
+        this.title = title;
+        this.director = director;
+
+        if (rating < 0.0) {
+            this.rating = 0.0;
+        } else if (rating > 10.0) {
+            this.rating = 10.0;
+        } else {
+            this.rating = rating;
+        }
+    }
+
+    public void displayDetails() {
+        System.out.println("Title: " + title + ", Director: " + director + ", Rating: " + rating);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int mode = sc.nextInt();
+        sc.nextLine();
+
+        Movie movie;
+
+        if (mode == 1) {
+            String title = sc.nextLine();
+            movie = new Movie(title);
+
+        } else if (mode == 2) {
+            String title = sc.nextLine();
+            String director = sc.nextLine();
+            movie = new Movie(title, director);
+
+        } else { // mode == 3
+            String title = sc.nextLine();
+            String director = sc.nextLine();
+            double rating = sc.nextDouble();
+            movie = new Movie(title, director, rating);
+        }
+
+        movie.displayDetails();
+    }
+}
